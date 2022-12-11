b79aa75fa89d6874b5ab9b83bae38821cc09478d
MarcinJaworski247 <marcinjaworski247@gmail.com>
2022-12-11 17:18:34 +0100

    build

--- quasar.config.js
+++ quasar.config.js
@@ -66,7 +66,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      distDir: "dist",
+      distDir: "docs",
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},

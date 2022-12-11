28180349e1e97d772e0850a88aaf84bf6bad7326
MarcinJaworski247 <marcinjaworski247@gmail.com>
2022-12-11 16:41:38 +0100

    dist dir change

--- quasar.config.js
+++ quasar.config.js
@@ -66,7 +66,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      distDir: "docs",
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
